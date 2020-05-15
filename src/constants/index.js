98bf2d103c30fad7d5e869ab33d5b4e575328781
kyralo <kyralo721@gmail.com>
2020-05-15 10:56:58 +0800

    加入oss

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+/*
+* @Author: 王宸
+* @Date:   2020-05-15 09:24:40
+* @Last Modified by:   王宸
+* @Last Modified time: 2020-05-15 10:56:36
+*/
+
+/**
+ * [cdn description] oss访问域名 
+ * @type {String}
+ */
+export const cdn = '';
